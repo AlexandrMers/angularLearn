1d46d3d076385f9650e6cac1b7bc423d71f361b7
Alexandr <alexandrAwdXY@gmail.com>
2020-02-05 21:46:29 +0300

    Разобрал основные моменты ангуляра 4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

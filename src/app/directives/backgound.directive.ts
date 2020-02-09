9469b3fa9719a7119276fde405822d2e64e35f3e
Alexandr <alexandrAwdXY@gmail.com>
2020-02-09 20:05:45 +0300

    Разобрал метод renderer при создании своей директивы.

--- src/app/directives/backgound.directive.ts
+++ src/app/directives/backgound.directive.ts
@@ -1,18 +1,21 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: "[appBackground]"
 })
 export class BackgoundDirective implements  OnInit {
   constructor(
-    private element: ElementRef
+    private element: ElementRef,
+    private renderer: Renderer2
   ) {
 
   }
 
   ngOnInit(): void {
-  this.element.nativeElement.style.backgroundColor = 'red';
-  }
+    const {nativeElement} = this.element;
+    this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+    this.renderer.addClass(nativeElement, 'white-text-el');
+}
 
 }
 

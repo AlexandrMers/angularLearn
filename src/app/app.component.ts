f5ecd859319b4c037deaf0c151d3cca66e1d067e
Alexandr <alexandrAwdXY@gmail.com>
2020-02-10 21:04:01 +0300

    Разобрал директиву ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public headerText: string = "Создание своей директивы";
+  public current: number = null;
+
+  public items = [1, 2, 3, 4, 5];
+
+  public onClick(val) {
+    this.current = val;
+  }
 }

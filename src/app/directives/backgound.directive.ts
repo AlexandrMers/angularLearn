e81880c0b0095d4bd0bef1c53adb441160f33f3c
Alexandr <alexandrAwdXY@gmail.com>
2020-02-10 20:35:52 +0300

    Разобрал метод hostListener и hostBinding

--- src/app/directives/backgound.directive.ts
+++ src/app/directives/backgound.directive.ts
@@ -1,21 +1,27 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: "[appBackground]"
 })
 export class BackgoundDirective implements  OnInit {
-  constructor(
-    private element: ElementRef,
-    private renderer: Renderer2
-  ) {
 
-  }
+
+  @HostBinding('style.backgroundColor') background: string;
+  constructor(
+    private element: ElementRef, // здесь мы определяем элемент, на который вешаем директиву.
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     const {nativeElement} = this.element;
     this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-    this.renderer.addClass(nativeElement, 'white-text-el');
 }
 
-}
+  @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) { // Таким образом можно прослушивать события на элементе, на который вешаем данную директиву.
+    this.background = 'red';
+  };
 
+  @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
+    this.background = 'blue';
+  };
+}

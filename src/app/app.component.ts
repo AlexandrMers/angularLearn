d2e9cbcac99a87debb816eaf020b8361e0b4c1d9
Alexandr <alexandrAwdXY@gmail.com>
2020-02-13 07:09:53 +0300

    Разобрал async pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import 'rxjs';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="container">
+      <h1>{{asyncTitle | async}}</h1>
       <input type="text" class="form-control mb-4 mt-4" [(ngModel)]="searchCar">
     <ul class="list-group">
       <li class="list-group-item" *ngFor="let car of cars | carFilter:searchCar; let idx = index"><b>{{idx + 1}} </b>{{car.name}}</li>
@@ -13,6 +16,8 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   public searchCar: string = "";
+
+  public asyncTitle = Observable.of('Async title 3seconds').delay(3000);
 
   cars = [
     {name: 'Ford'},

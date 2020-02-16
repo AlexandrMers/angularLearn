2be49fae44abc849159cb799f026a48020eb6309
Alexandr <alexandrAwdXY@gmail.com>
2020-02-16 13:26:25 +0300

    Создание формы. Рассмотрел template driving подход в создании формы. Обработка формы через ангуляр.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 interface AnswerInterface {
   type: string;
@@ -22,5 +23,9 @@ export class AppComponent   {
       text: 'нет'
     }
   ];
+
+  public submitForm(form: NgForm) {
+    console.log('submitted form ', form);
+  }
 
 }

a08ebbda223f35ab57b13ea52954ed2de9932b96
Alexandr <alexandrAwdXY@gmail.com>
2020-02-18 06:08:32 +0300

    Рассмотрел создание асинхронного валидатора.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,8 @@ export class AppComponent implements OnInit {
     // Инициализация формы...
     this.form = new FormGroup({
       user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email], this.checkMatchEmail), // Асинхронные валидаторы передаются третьим аргументом!
+        password: new FormControl('', [Validators.required, this.checkForLength]),
       }),
       country: new FormControl('ru'),
       answer: new FormControl('no')
@@ -40,5 +40,25 @@ export class AppComponent implements OnInit {
   public submitForm() {
     console.log('submitted ', this.form);
   };
+
+  public checkForLength(control: FormControl) {
+    if(control.value.length <= 6) {
+      return {
+        'errorLength': true
+      }
+    }
+    return null;
+  }
+
+  public checkMatchEmail(control: FormControl): Promise<any> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if(control.value === 'test@mail.ru') resolve({
+          isUsed: true
+        });
+        resolve(null);
+      }, 3000);
+    });
+  }
 }
 
